3c0e066ee936d1f4088ead4b033977e2c029bcfa
lis0517 <01034895017a@gmail.com>
2024-07-02 21:05:27 +0900

    update PostRepositoryCustom
    자신이 좋아요한 게시물 가져오기

--- src/main/java/com/twelve/challengeapp/repository/impl/PostRepositoryCustomImpl.java
+++ src/main/java/com/twelve/challengeapp/repository/impl/PostRepositoryCustomImpl.java
@@ -1,10 +1,15 @@
 package com.twelve.challengeapp.repository.impl;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.twelve.challengeapp.dto.PostResponseDto;
 import com.twelve.challengeapp.entity.QPost;
@@ -36,5 +41,31 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 			.where(post.id.eq(postId))
 			.groupBy(post.id)
 			.fetchOne());
+	}
+
+	@Override
+	public Page<PostResponseDto> findLikedPostsByUserId(Long userId, Pageable pageable) {
+		QPost post = QPost.post;
+		QPostLike postLike = QPostLike.postLike;
+
+		List<PostResponseDto> posts = queryFactory.select(
+			Projections.constructor(PostResponseDto.class,post.id, post.user.username, post.title, post.content,
+				post.createdAt, post.updatedAt, postLike.id.count().intValue()))
+			.from(postLike)
+			.join(postLike.post, post)
+			.where(postLike.user.id.eq(userId))
+			.groupBy(post.id)
+			.orderBy(post.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		JPAQuery<Long> countQuery = queryFactory.select(
+			post.id.countDistinct())
+			.from(postLike)
+			.join(postLike.post, post)
+			.where(postLike.user.id.eq(userId));
+
+		return PageableExecutionUtils.getPage(posts, pageable, countQuery::fetchOne);
 	}
 }
