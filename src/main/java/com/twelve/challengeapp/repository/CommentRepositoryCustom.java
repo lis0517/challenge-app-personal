b2038cc8552220eb4c7a7ba8cdfaa65096d01089
lis0517 <01034895017a@gmail.com>
2024-07-03 11:12:14 +0900

    update CommentRepositoryCustom
    좋아요 한 댓글 목록 조회 기능 추가

--- src/main/java/com/twelve/challengeapp/repository/CommentRepositoryCustom.java
+++ src/main/java/com/twelve/challengeapp/repository/CommentRepositoryCustom.java
@@ -2,10 +2,14 @@ package com.twelve.challengeapp.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.twelve.challengeapp.dto.CommentResponseDto;
 
 public interface CommentRepositoryCustom {
 
 	Optional<CommentResponseDto> findCommentWithLikeCount(Long commentId);
 
+	Page<CommentResponseDto> findLikedCommentsByUserId(Long userId, Pageable pageable);
 }
--- src/main/java/com/twelve/challengeapp/repository/impl/CommentRepositoryCustomImpl.java
+++ src/main/java/com/twelve/challengeapp/repository/impl/CommentRepositoryCustomImpl.java
@@ -1,10 +1,15 @@
 package com.twelve.challengeapp.repository.impl;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.twelve.challengeapp.dto.CommentResponseDto;
 import com.twelve.challengeapp.entity.QComment;
@@ -29,13 +34,40 @@ public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
 		QCommentLike commentLike = QCommentLike.commentLike;
 
 		return Optional.ofNullable(queryFactory.select(
-				Projections.constructor(CommentResponseDto.class, comment.id, comment.user.username, comment.content, comment.createdAt,
-					comment.updatedAt, commentLike.id.count().intValue()))
+				Projections.constructor(CommentResponseDto.class, comment.id, comment.user.username, comment.content,
+					comment.createdAt, comment.updatedAt, commentLike.id.count().intValue()))
 			.from(comment)
 			.leftJoin(commentLike)
 			.on(commentLike.comment.id.eq(comment.id))
 			.where(comment.id.eq(commentId))
 			.groupBy(comment.id)
 			.fetchOne());
+	}
+
+	@Override
+	public Page<CommentResponseDto> findLikedCommentsByUserId(Long userId, Pageable pageable) {
+
+		QComment comment = QComment.comment;
+		QCommentLike commentLike = QCommentLike.commentLike;
+
+		List<CommentResponseDto> comments = queryFactory.select(
+				Projections.constructor(CommentResponseDto.class, comment.id, comment.user.username, comment.content,
+					comment.createdAt, comment.updatedAt, commentLike.id.count().intValue()))
+			.from(commentLike)
+			.join(commentLike.comment, comment)
+			.where(commentLike.user.id.eq(userId))
+			.groupBy(comment.id)
+			.orderBy(comment.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		JPAQuery<Long> countQuery = queryFactory.select(comment.id.countDistinct())
+			.from(commentLike)
+			.join(commentLike.comment, comment)
+			.where(commentLike.user.id.eq(userId))
+			.groupBy(comment.id);
+
+		return PageableExecutionUtils.getPage(comments, pageable, countQuery::fetchOne);
 	}
 }
