9ae32469e48367d3fd4fba8624797fbb8ad7d380
lis0517 <01034895017a@gmail.com>
2024-07-03 21:03:13 +0900

    update Dto
    좋아요 내용 관련 추가

--- src/main/java/com/twelve/challengeapp/dto/UserResponseDto.java
+++ src/main/java/com/twelve/challengeapp/dto/UserResponseDto.java
@@ -2,23 +2,41 @@ package com.twelve.challengeapp.dto;
 
 import com.twelve.challengeapp.entity.User;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDto {
 	private String username;
 	private String nickname;
 	private String introduce;
 	private String email;
+	private Long postLike;
+	private Long commentLike;
+
+	@Builder
+	public UserResponseDto(String username, String nickname, String introduce, String email) {
+		this.username = username;
+		this.nickname = nickname;
+		this.introduce = introduce;
+		this.email = email;
+		this.postLike = 0L;
+		this.commentLike = 0L;
+	}
 
 	public UserResponseDto(User user) {
 		this.username = user.getUsername();
 		this.nickname = user.getNickname();
 		this.introduce = user.getIntroduce();
 		this.email = user.getEmail();
+		this.postLike = 0L;
+		this.commentLike = 0L;
+	}
+
+	public void setLikedCounts(Long postLike, Long commentLike) {
+		this.postLike = postLike;
+		this.commentLike = commentLike;
 	}
 }
